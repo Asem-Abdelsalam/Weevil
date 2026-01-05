afd0517fcb4a45ff67aa09125fb8bbebd7988f39
Asem Abdelsalam <assemmohamed1000@gmail.com>
2026-01-05 16:52:31 +0200

    Add project files.

---
+++ Core/WeevilConstants.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weevil.Core
+{
+    // Defines Plugin Constants
+    public static class WeevilConstants
+    {
+        // Main tab name
+        public const string Category = "Weevil";
+
+        // Subcategories
+        public const string Curves = "CurveProcessing";
+        public const string Nesting = "Nesting";
+        public const string Toolpaths = "Toolpaths";
+        public const string GCode = "GCode";
+
+    }
+}
